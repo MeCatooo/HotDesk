7590d7c2f84e5bb271b3565126ce269726d2cf29
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-06-11 17:23:57 +0200

    Test project

---
+++ HotDeskTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+using HotDesk.Controllers;
+using HotDesk.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HotDeskTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private HotDeskRepository GetContext()
+        {
+            var options = new DbContextOptionsBuilder<HotDeskDbContext>().UseInMemoryDatabase("HotDesk")
+                .Options;
+            var databaseContext = new HotDeskDbContext(options);
+            databaseContext.Database.EnsureCreated();
+            var repository = new HotDeskRepository(databaseContext);
+            return repository;
+        }
+        [TestMethod]
+        public void AddLocationTest()
+        {
+            var repository = GetContext();
+            var controller = new LocationsController(repository);
+            var result = controller.Create("Kraków");
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            Assert.AreEqual((result as OkObjectResult).Value, repository.GetLocation(1));
+        }
+    }
+}
