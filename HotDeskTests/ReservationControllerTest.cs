d94bf5e780eec26e3d34143b7612fa11e1babe80
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-06-12 18:49:54 +0200

    Unit tests

--- HotDeskTests/ReservationControllerTest.cs
+++ HotDeskTests/ReservationControllerTest.cs
@@ -1,9 +1,11 @@
 ﻿using HotDesk.Controllers;
 using HotDesk.Models;
+using JwtApp.Controllers;
+using JwtApp.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,22 +35,23 @@ namespace HotDeskTests
             var result = controller.Index() as OkObjectResult;
             Assert.IsNotNull(result.Value);
         }
-        //[TestMethod]
-        //public void AddReservationFree()
-        //{
-        //    var repository = GetContext();
-        //    repository.AddUser(new JwtApp.Models.UserLogin() { Username = "login", Password = "123" });
-            
-        //    //var controllerLocations = new LocationsController(repository);
-        //    var controller = new ReservationsController(repository);
-        //    var mock = new Mock<ControllerContext>();
 
-        //    mock.SetupGet(x => x.HttpContext.User.Identity).Returns( new ClaimsIdentity() { Claims =});
-        //    controller.ControllerContext = mock.Object;
-        //    //controllerLocations.Create("Warszawa");
-        //    //controllerLocations.AddDesk(1,"NextToWindow");
-        //    var result = controller.Create(1,1, new TimeStamps() { From=new DateTime(2023,1,1), To = new DateTime(2023, 1, 3) }) as OkObjectResult;
-        //    Assert.IsNotNull(result.Value);
-        //}
+        [TestMethod]
+        public void AddReservationFree()
+        {
+            var repository = GetContext();
+            repository.AddUser(new JwtApp.Models.UserLogin() { Username = "login", Password = "123" });
+
+            var controllerLocations = new LocationsController(repository);
+            var controller = new ReservationsController(repository);
+            var controllerLogin = new LoginController(repository);
+            controller.ControllerContext = new ControllerContext();
+            controllerLocations.Create("Warszawa");
+            controllerLocations.AddDesk(1,"NextToWindow");
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers.Authorization = "Bearer "+controllerLogin.Login(new UserLogin() { Username = "login", Password = "123" });
+            var result = controller.Create(1, 1, new TimeStamps() { From = new DateTime(2023, 1, 1), To = new DateTime(2023, 1, 3) }) as OkObjectResult;
+            Assert.IsTrue((result.Value as Reservation).desk.Name== "NextToWindow");
+        }
     }
 }
