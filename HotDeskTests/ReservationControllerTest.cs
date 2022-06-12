16a8ff644df55f07f3082a9aa591e2af74e475f8
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-06-12 19:11:17 +0200

    Unit tests

--- HotDeskTests/ReservationControllerTest.cs
+++ HotDeskTests/ReservationControllerTest.cs
@@ -28,7 +28,7 @@ namespace HotDeskTests
             return repository;
         }
         [TestMethod]
-        public void GetAllReservations()
+        public void GetAllReservationsTest()
         {
             var repository = GetContext();
             var controller = new ReservationsController(repository);
@@ -36,22 +36,53 @@ namespace HotDeskTests
             Assert.IsNotNull(result.Value);
         }
 
-        //[TestMethod]
-        //public void AddReservationFree()
-        //{
-        //    var repository = GetContext();
-        //    repository.AddUser(new JwtApp.Models.UserLogin() { Username = "login", Password = "123" });
+        [TestMethod]
+        public void AddReservationFreeTest()
+        {
+            var repository = GetContext();
+            repository.AddUser(new UserLogin() { Username = "login", Password = "123" });
+            var controller = new ReservationsController(repository);
+            var controllerLogin = new LoginController(repository);
 
-        //    var controllerLocations = new LocationsController(repository);
-        //    var controller = new ReservationsController(repository);
-        //    var controllerLogin = new LoginController(repository);
-        //    controllerLocations.Create("Warszawa");
-        //    controllerLocations.AddDesk(1,"NextToWindow");
-        //    controller.ControllerContext = new ControllerContext();
-        //    controller.ControllerContext.HttpContext = new DefaultHttpContext();
-        //    controller.ControllerContext.HttpContext.Request.Headers.Authorization = "Bearer "+controllerLogin.Login(new UserLogin() { Username = "login", Password = "123" });
-        //    var result = controller.Create(1, 1, new TimeStamps() { From = new DateTime(2023, 1, 1), To = new DateTime(2023, 1, 3) }) as OkObjectResult;
-        //    Assert.IsTrue((result.Value as Reservation).desk.Name== "NextToWindow");
-        //}
+            var location = repository.AddLocation(new Location() { Name = "Kraków" });
+            var desk = repository.AddDesk(new Desk() { Name = "NextToWindow", Location = location });
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers.Authorization = "Bearer " + controllerLogin.Login(new UserLogin() { Username = "login", Password = "123" });
+            var result = controller.Create(location.Id, desk.Id, new TimeStamps() { From = new DateTime(2023, 1, 1), To = new DateTime(2023, 1, 3) }) as OkObjectResult;
+            Assert.IsTrue((result.Value as Reservation).desk.Name == "NextToWindow");
+        }
+        [TestMethod]
+        public void AddReservationFree_OccupiedTest()
+        {
+            var repository = GetContext();
+            var user = repository.AddUser(new UserLogin() { Username = "Fernando", Password = "123" });
+            var controller = new ReservationsController(repository);
+            var controllerLogin = new LoginController(repository);
+
+            var location = repository.AddLocation(new Location() { Name = "Kraków" });
+            var desk = repository.AddDesk(new Desk() { Name = "NextToWindow", Location = location });
+            var reservation = repository.AddReservation(new Reservation() { From = new DateTime(2023, 1, 1), To = new DateTime(2023, 1, 4), desk = desk, location = location, user = user });
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers.Authorization = "Bearer " + controllerLogin.Login(new UserLogin() { Username = user.Username, Password = user.Password });
+            var result = controller.Create(location.Id, desk.Id, new TimeStamps() { From = new DateTime(2023, 1, 1), To = new DateTime(2023, 1, 3) });
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+        }
+        [TestMethod]
+        public void ChangeDeskTest()
+        {
+            var repository = GetContext();
+            var user = repository.AddUser(new UserLogin() { Username = "Alberto", Password = "123" });
+            var controller = new ReservationsController(repository);
+
+            var location = repository.AddLocation(new Location() { Name = "Kraków" });
+            var desk = repository.AddDesk(new Desk() { Name = "NextToWindow", Location = location });
+            var desk1 = repository.AddDesk(new Desk() { Name = "NextToWindow1", Location = location });
+            var reservation = repository.AddReservation(new Reservation() { From = new DateTime(2023, 1, 1), To = new DateTime(2023, 1, 4), desk = desk, location = location, user = user });
+            var result = controller.Edit(reservation.Id, desk1.Id) as OkObjectResult;
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            Assert.IsTrue((result.Value as Reservation).desk.Name == "NextToWindow1");
+        }
     }
 }
